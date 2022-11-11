b73678ddc62fdec81b597f04cfbb8b744424a00a
Juan Castorena <03juan.c.castorena@gmail.com>
2022-11-11 12:28:19 -0600

    Connected Backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import axios from "axios";
+import baseUrl from './config'
 
 const App = () => {
-    return <div>My first React App</div>;
+
+    axios.get(`${baseUrl}/api/v1/books`).then(books => {
+        console.log(books);
+    });
+
+    return <div>React App</div>;
 };
 
 
---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const environmentUrls = new Map();
+
+
+
+environmentUrls.set('localhost', 'http://localhost:8080');
+environmentUrls.set('book-store-web-v2.herokuapp.com', 'https://book-store-web-v2.herokuapp.com');
+
+
+export default environmentUrls.get(window.location.hostname);
