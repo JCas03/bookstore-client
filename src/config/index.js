ffd316a6d28b6177a5e0934141a53e9d7351fa27
Juan Castorena <03juan.c.castorena@gmail.com>
2022-11-21 10:50:52 -0600

    env url fix

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const environmentUrls = new Map();
 
 
 environmentUrls.set('localhost', 'http://localhost:8080');
-environmentUrls.set('book-store-web-v2.herokuapp.com', 'https://book-store-web-v2.herokuapp.com');
+environmentUrls.set('book-store-v2.herokuapp.com', 'https://book-store-v2.herokuapp.com');
 
 
 export default environmentUrls.get(window.location.hostname);
